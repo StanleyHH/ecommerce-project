b8d87b4f16159c00c425477091b0a4755ee843c3
Stanislav Dering <stanislavdering@gmail.com>
2025-09-24 06:37:01 +0200

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,13 @@ import Checkmark from '../assets/images/icons/checkmark.png';
 import './homePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+        console.log(data)
+    });
+
     return (
         <>
             <title>Ecommerce Project</title>
