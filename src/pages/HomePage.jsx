4ff5d8200abb0f5416b2e55888bc7b41496e28ac
Stanislav Dering <stanislavdering@gmail.com>
2025-09-24 07:09:51 +0200

    Use data from the backend to generate the html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
-import { Header } from '../components/Header.jsx';
-import { products } from '../../starting-code/data/products.js';
-import Checkmark from '../assets/images/icons/checkmark.png';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Header } from '../components/Header.jsx';
+import Checkmark from '../assets/images/icons/checkmark.png';
 
 import './homePage.css';
 
 export function HomePage() {
-    axios.get('http://localhost:3000/api/products')
-        .then((response) => {
-            console.log(response.data);
-        });
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data);
+            });
+    }, []);
 
     return (
         <>
